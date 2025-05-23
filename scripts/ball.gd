170fdae693fb00cbf10f2d496aeb3d827daaafb0
Archaros1 <arthurjung2@hotmail.fr>
2025-05-23 23:04:22 +0200

    ball bouncing

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -3,6 +3,10 @@ extends CharacterBody2D
 
 const START_SPEED = 100.0
 const START_ANGLE = 45.0
+@onready var ray_cast_top: RayCast2D = $RayCastTop
+@onready var ray_cast_right: RayCast2D = $RayCastRight
+@onready var ray_cast_down: RayCast2D = $RayCastDown
+@onready var ray_cast_left: RayCast2D = $RayCastLeft
 
 func _physics_process(delta: float) -> void:
 	var gameOn = false
@@ -22,12 +26,48 @@ func _physics_process(delta: float) -> void:
 	var speed = get_meta('currentSpeed')
 	var angle = get_meta('currentAngle')
 	
+	angle = update_angle_on_collision(angle)
+	
 	# Once we start moving.
 	var vector = get_direction_from_rota(angle)
 	velocity.x = vector.x * speed
 	velocity.y = vector.y * speed
 
 	move_and_slide()
+	
+	set_meta('currentSpeed', speed)
+	set_meta('currentAngle', angle)
 
 func get_direction_from_rota(angle: float) -> Dictionary:
-	return {'x': cos(angle), 'y': sin(angle)}
+	return {'x': cos(deg_to_rad(angle)), 'y': sin(deg_to_rad(angle))}
+
+func update_angle_on_collision(angle: float) -> float:
+	var collision_x_disabled = false
+	var collision_y_disabled = false
+	
+	# Remember previous frame.
+	if has_meta('collision_x_disabled'):
+		collision_x_disabled = get_meta('collision_x_disabled')
+	if has_meta('collision_y_disabled'):
+		collision_y_disabled = get_meta('collision_y_disabled')
+	
+	# If collision enabled & colliding.
+	if !collision_x_disabled && (ray_cast_right.is_colliding() || ray_cast_left.is_colliding()):
+		set_meta('collision_x_disabled', true)
+		angle = 180 - angle
+	if !collision_y_disabled && (ray_cast_top.is_colliding() || ray_cast_down.is_colliding()):
+		set_meta('collision_y_disabled', true)
+		angle *= -1
+	
+	# Enable collisions if not colliding.
+	if collision_x_disabled && !ray_cast_right.is_colliding() && !ray_cast_left.is_colliding():
+		set_meta('collision_x_disabled', false)
+	if collision_y_disabled && !ray_cast_top.is_colliding() && !ray_cast_down.is_colliding():
+		set_meta('collision_y_disabled', false)
+	
+	return angle
+	
+	
+	
+	
+	
